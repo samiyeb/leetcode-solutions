c9b2156f2e25e798a9d5b9ae34dc20445cb030a9
Sam Bakri <jerry.monclaire@outlook.com>
2025-01-04 19:43:09 -0600

    1614. Maximum Nesting Depth of the Parentheses

---
+++ string/maximum_nesting_depth_of_the_parentheses.cpp.cpp
@@ -0,0 +1,23 @@
+#include <climits>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count = 0;
+        int res = INT_MIN;
+
+        for (char c : s) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+
+            res = max(res, count);
+        }
+
+        return res;
+        
+    }
+};
